7364874a8e3e0dc4c0c249ff31590ae7b87811b2
Neeraj Gupta <neeraj.gmf@gmail.com>
2024-11-22 00:47:13 +0530

    fixed login page design

--- app/login/components/Login.tsx
+++ app/login/components/Login.tsx
@@ -136,6 +136,34 @@ const LoginPage: React.FC<LoginPageProps> = ({ host, searchParams, params }) => 
               <h2 className="text-2xl font-semibold text-gray-800 mt-4 font-jakarta">Log in / Sign up</h2>
             </div>
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+              <div className="relative">
+                <Button
+                  type="button"
+                  onClick={handleGoogleSignIn}
+                  className="w-[339.5px] h-[46px] px-[16.5px] py-[13px] bg-[#5B16FE] text-white font-medium rounded-[50px] border-t border-l hover:bg-[#4A12CC] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#5B16FE] flex items-center justify-center"
+                >
+                  <svg className="w-5 h-5 mr-2" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20.3081 10.2303C20.3081 9.55056 20.253 8.86711 20.1354 8.19836H10.7031V12.0492H16.1046C15.8804 13.2911 15.1602 14.3898 14.1057 15.0879V17.5866H17.3282C19.2205 15.8449 20.3081 13.2728 20.3081 10.2303Z" fill="#3F83F8"/>
+                    <path d="M10.7019 20.0006C13.3989 20.0006 15.6734 19.1151 17.3306 17.5865L14.1081 15.0879C13.2115 15.6979 12.0541 16.0433 10.7056 16.0433C8.09669 16.0433 5.88468 14.2832 5.091 11.9169H1.76562V14.4927C3.46322 17.8695 6.92087 20.0006 10.7019 20.0006V20.0006Z" fill="#34A853"/>
+                    <path d="M5.08857 11.9169C4.66969 10.6749 4.66969 9.33008 5.08857 8.08811V5.51233H1.76688C0.348541 8.33798 0.348541 11.667 1.76688 14.4927L5.08857 11.9169V11.9169Z" fill="#FBBC04"/>
+                    <path d="M10.7019 3.95805C12.1276 3.936 13.5055 4.47247 14.538 5.45722L17.393 2.60218C15.5852 0.904587 13.1858 -0.0287217 10.7019 0.000673888C6.92087 0.000673888 3.46322 2.13185 1.76562 5.51234L5.08732 8.08813C5.87733 5.71811 8.09302 3.95805 10.7019 3.95805V3.95805Z" fill="#EA4335"/>
+                  </svg>
+                  Continue with Google
+                </Button>
+                <span className="absolute -top-2 -left-2 px-2 py-1 text-xs font-jakarta text-white bg-blue-600">
+                  Most Used
+                </span>
+              </div>
+
+              <div className="relative my-6">
+                <div className="absolute inset-0 flex items-center">
+                  <div className="w-full border-t border-gray-300"></div>
+                </div>
+                <div className="relative flex justify-center text-sm">
+                  <span className="px-2 text-gray-500 bg-white">Or continue with email</span>
+                </div>
+              </div>
+
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1 font-jakarta">
                   Email
@@ -159,24 +187,10 @@ const LoginPage: React.FC<LoginPageProps> = ({ host, searchParams, params }) => 
               </div>
               
               <Button
-                type="button"
-                onClick={handleGoogleSignIn}
-                className="w-[339.5px] h-[46px] px-[16.5px] py-[13px] bg-[#5B16FE] text-white font-medium rounded-[50px] border-t border-l hover:bg-[#4A12CC] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#5B16FE] flex items-center justify-center"
-              >
-                <svg className="w-5 h-5 mr-2" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M20.3081 10.2303C20.3081 9.55056 20.253 8.86711 20.1354 8.19836H10.7031V12.0492H16.1046C15.8804 13.2911 15.1602 14.3898 14.1057 15.0879V17.5866H17.3282C19.2205 15.8449 20.3081 13.2728 20.3081 10.2303Z" fill="#3F83F8"/>
-                  <path d="M10.7019 20.0006C13.3989 20.0006 15.6734 19.1151 17.3306 17.5865L14.1081 15.0879C13.2115 15.6979 12.0541 16.0433 10.7056 16.0433C8.09669 16.0433 5.88468 14.2832 5.091 11.9169H1.76562V14.4927C3.46322 17.8695 6.92087 20.0006 10.7019 20.0006V20.0006Z" fill="#34A853"/>
-                  <path d="M5.08857 11.9169C4.66969 10.6749 4.66969 9.33008 5.08857 8.08811V5.51233H1.76688C0.348541 8.33798 0.348541 11.667 1.76688 14.4927L5.08857 11.9169V11.9169Z" fill="#FBBC04"/>
-                  <path d="M10.7019 3.95805C12.1276 3.936 13.5055 4.47247 14.538 5.45722L17.393 2.60218C15.5852 0.904587 13.1858 -0.0287217 10.7019 0.000673888C6.92087 0.000673888 3.46322 2.13185 1.76562 5.51234L5.08732 8.08813C5.87733 5.71811 8.09302 3.95805 10.7019 3.95805V3.95805Z" fill="#EA4335"/>
-                </svg>
-                Continue with Google
-              </Button>
-              
-              <Button
                 type="submit"
                 disabled={isSubmitting}
                 variant="outline"
-                className="w-[339.5px] h-[46px] px-[16.5px] py-[13px] bg-white text-gray-700 font-medium rounded-[50px] border-t border-l hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 flex items-center justify-center"
+                className="w-[339.5px] h-[46px] px-[16.5px] py-[13px] bg-white text-[#5B16FE] font-medium rounded-[50px] border border-[#5B16FE] hover:bg-[#5B16FE]/5 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#5B16FE] flex items-center justify-center transition-colors"
               >
                 Get code on E-mail
                 <ArrowRight className="ml-2 w-4 h-4" />
@@ -185,7 +199,7 @@ const LoginPage: React.FC<LoginPageProps> = ({ host, searchParams, params }) => 
             
             <div className="text-center mt-6">
               <p className="w-[314.95px] h-[34px] text-xs text-gray-500 font-poppins font-normal leading-[18px] mx-auto">
-                Click the button, you will receive a one time code in<br />your inbox.
+                You will receive a one time code in your inbox.
               </p>
             </div>
           </div>
