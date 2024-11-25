d195e4b07bb7ce70cd6d7653f68659bdb4408e0a
kanu6284 <bkanika189@gmail.com>
2024-11-25 11:30:42 +0530

    add vikings page

--- components/Why.tsx
+++ components/Why.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 // Add a props interface
 interface WhyProps {
-  imageSet?: 'dating' | 'doctor' | 'Jfram' | 'ColorFram' | 'GlmFram' | 'FitFram' | 'tubefram'  | 'Group6' | 'Group7' | 'Group8' | 'Group9' | 'Group10' | 'Group11' | 'Group12' | 'Group13' | 'Group14' | 'Group15' | 'Group16';  // default will be 'dating'
+  imageSet?: 'dating' | 'doctor' | 'Jfram' | 'ColorFram' | 'GlmFram' | 'FitFram' | 'tubefram'  | 'Group6' | 'Group7' | 'Group8' | 'Group9' | 'Group10' | 'Group11' | 'Group12' | 'Group13' | 'Group14' | 'Group15' | 'Group16' | 'Group17';  // default will be 'dating'
 }
 
 const Why = ({ imageSet = 'dating' }: WhyProps) => {
@@ -49,6 +49,7 @@ const Why = ({ imageSet = 'dating' }: WhyProps) => {
     Group14:"/Group14.png",
     Group15:"/Group15.png",
     Group16:"/Group16.png",
+    Group17:"/Group17.png",
   };
 
   return (
