429951cd739bad25e266390a08dd01aacbad6ad1
kanu6284 <bkanika189@gmail.com>
2025-01-03 20:02:24 +0530

    differnt packs pages

--- components/BrandPage.tsx
+++ components/BrandPage.tsx
@@ -60,48 +60,36 @@ const BrandingPage = () => {
   return (
     <div className="w-full bg-white">
       
-      <div className="w-full bg-[#F6F2FF] overflow-hidden mb-12">
-        <div className="relative w-[1440px] h-[90px] mx-auto">
+      <div className="w-full bg-[#F6F2FF] overflow-hidden mb-6">
+        <div className="relative w-full md:w-[1440px] h-[50px] md:h-[80px] mx-auto py-2 md:py-4">
           <div className="absolute top-0 left-0 w-full h-full flex items-center">
             {/* First set of brands */}
-            <div className="flex animate-marquee items-center gap-12 whitespace-nowrap">
+            <div className="flex animate-marquee items-center gap-3 md:gap-8 whitespace-nowrap">
               {brands.map((brand) => (
                 <div 
                   key={brand.id}
-                  className="flex-shrink-0 flex items-center justify-center"
-                  style={{ 
-                    width: `${brand.width}px`,
-                    height: `${brand.height}px`
-                  }}
+                  className="flex-shrink-0 flex items-center justify-center w-[184px] h-[50px] md:w-[307px] md:h-[83px]"
                 >
                   <img
                     src={brand.logo}
                     alt={`${brand.name} logo`}
                     className="w-full h-full object-contain grayscale hover:grayscale-0 transition-all duration-300"
-                    width={brand.width}
-                    height={brand.height}
                   />
                 </div>
               ))}
             </div>
 
             {/* Duplicate set */}
-            <div className="flex animate-marquee items-center gap-12 whitespace-nowrap">
+            <div className="flex animate-marquee items-center gap-3 md:gap-8 whitespace-nowrap">
               {brands.map((brand) => (
                 <div 
                   key={`${brand.id}-duplicate`}
-                  className="flex-shrink-0 flex items-center justify-center"
-                  style={{ 
-                    width: `${brand.width}px`,
-                    height: `${brand.height}px`
-                  }}
+                  className="flex-shrink-0 flex items-center justify-center w-[184px] h-[50px] md:w-[307px] md:h-[83px]"
                 >
                   <img
                     src={brand.logo}
                     alt={`${brand.name} logo`}
                     className="w-full h-full object-contain grayscale hover:grayscale-0 transition-all duration-300"
-                    width={brand.width}
-                    height={brand.height}
                   />
                 </div>
               ))}
